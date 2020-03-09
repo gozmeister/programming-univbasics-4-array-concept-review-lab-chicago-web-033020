26968adfbad10e19a9e0c8355ab1a2406c2c7afa
Sam Gorick <samgorick.sg@gmail.com>
2020-03-09 16:02:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,17 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  length = array.length
+  counter = 0
+  max_value = 0
+  while counter < length do
+    if array[counter] > max_value
+      max_value = array[counter]
+      counter +=1
+    else counter +=1
+    end
+  end
+  max_value
 end
 
 def find_min_value(array)
